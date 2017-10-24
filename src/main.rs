7cbd58acb11e353ad2c8410a1357f74d4b8c6ef5
4hiziri <meirvg@gmail.com>
2017-10-25 00:30:22 +0900

    Add Inst Display

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,47 @@ enum Inst {
     Dec,
     Put,
     Get,
-    Begin,
+    Bgn,
     End,
+    Nop,
+}
+
+impl std::fmt::Display for Inst {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let repr = match self {
+            PInc => "pinc",
+            PDec => "pdec",
+            Inc => "inc",
+            Dec => "dec",
+            Put => "put",
+            Get => "get",
+            Bgn => "bgn",
+            End => "end",
+            Nop => "nop",
+        };
+
+        write!(f, "{}", repr)
+    }
 }
 
 named!(
     parser,
     alt!(tag!(">") | tag!("<") | tag!("+") | tag!("-") | tag!(".") | tag!(",") | tag!("[") | tag!("]"))
 );
+
+fn str2inst(symbol: &str) -> Inst {
+    match symbol {
+        ">" => Inst::PInc,
+        "<" => Inst::PDec,
+        "+" => Inst::Inc,
+        "-" => Inst::Dec,
+        "." => Inst::Put,
+        "," => Inst::Get,
+        "[" => Inst::Bgn,
+        "]" => Inst::End,
+        _ => Inst::Nop,
+    }
+}
 
 fn parse(input: &str) -> (Inst, &[u8]) {
     match parser(input.as_bytes()) {
@@ -33,18 +66,7 @@ fn main() {
     let mut pointer: u8 = 0;
     let mut memory: [u8; 256] = [0; 256];
 
-    // match parser(sample.as_bytes()) {
-    //     IResult::Done(i, o) => {
-    //         println!(
-    //             "{}, {}",
-    //             String::from_utf8(i.to_vec()).unwrap(),
-    //             String::from_utf8(o.to_vec()).unwrap(),
-    //         )
-    //     }
-    //     _ => println!("Other!\n"),
-    // };
-
     let (inst, rest) = parse(sample);
 
-    println!("{}", std::str::from_utf8(rest).unwrap());
+    println!("{}", str2inst(std::str::from_utf8(rest).unwrap()));
 }
