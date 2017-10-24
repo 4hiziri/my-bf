3faf14656806167916891f462b64d63e2d8998ab
tkgsy <meirvg@gmail.com>
2017-10-24 23:29:52 +0900

    Add sample

--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,36 @@ enum Inst {
 }
 
 named!(
-    pinc,
+    parser,
     alt!(tag!(">") | tag!("<") | tag!("+") | tag!("-") | tag!(".") | tag!(",") | tag!("[") | tag!("]"))
 );
 
-fn parse(input: &[u8]) -> (Inst, &[u8]) {}
+fn parse(input: &str) -> (Inst, &[u8]) {
+    match parser(input.as_bytes()) {
+        IResult::Done(i, o) => (Inst::End, o),
+        _ => (Inst::End, &[]),
+    }
+}
 
 // Load all instruction at beginning?
 
 fn main() {
-    let sample = "++++++++++++++++++++++++++.";
+    let sample: &str = "++++++++++++++++++++++++++.";
     let mut pointer: u8 = 0;
     let mut memory: [u8; 256] = [0; 256];
 
+    // match parser(sample.as_bytes()) {
+    //     IResult::Done(i, o) => {
+    //         println!(
+    //             "{}, {}",
+    //             String::from_utf8(i.to_vec()).unwrap(),
+    //             String::from_utf8(o.to_vec()).unwrap(),
+    //         )
+    //     }
+    //     _ => println!("Other!\n"),
+    // };
 
-    match pinc(sample.as_bytes()) {
-        IResult::Done(i, o) => {
-            println!(
-                "{}, {}",
-                String::from_utf8(i.to_vec()).unwrap(), // world!
-                String::from_utf8(o.to_vec()).unwrap(), // Hello, 
-            )
-        }
-        _ => println!("Other!\n"),
-    };
+    let (inst, rest) = parse(sample);
+
+    println!("{}", std::str::from_utf8(rest).unwrap());
 }
